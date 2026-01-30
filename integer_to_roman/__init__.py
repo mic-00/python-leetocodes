9a1bf3312f9c117a0bb3e86b4a2c7989f061c3f6
mic-00 <michele.masetto@gmail.com>
2026-01-30 10:49:36 +0100

    Solve LeetCode "Integer to Roman" (https://leetcode.com/problems/integer-to-roman)

--- integer_to_roman/__init__.py
+++ integer_to_roman/__init__.py
@@ -7,18 +7,18 @@ class Solution(object):
         i = int(math.log10(num))
         d = int(num // 10 ** i)
         if d < 4:
-            s = self._getSymbol(i) * d
+            s = self.__getSymbol(i) * d
         elif d == 4:
-            s = self._getSymbolCase4(i)
+            s = self.__getSymbolCase4(i)
         elif d == 5:
-            s = self._getSymbolCase5(i)
+            s = self.__getSymbolCase5(i)
         elif d < 9:
-            s = self._getSymbolCase5(i) + self._getSymbol(i) * (d - 5)
+            s = self.__getSymbolCase5(i) + self.__getSymbol(i) * (d - 5)
         else:
-            s = self._getSymbolCase9(i)
+            s = self.__getSymbolCase9(i)
         return s + self.intToRoman(num - d * 10 ** i)
     
-    def _getSymbol(self, i: int) -> str:
+    def __getSymbol(self, i: int) -> str:
         if i == 0:
             return "I"
         if i == 1:
@@ -27,21 +27,21 @@ class Solution(object):
             return "C"
         return "M"
     
-    def _getSymbolCase4(self, i: int) -> str:
+    def __getSymbolCase4(self, i: int) -> str:
         if i == 0:
             return "IV"
         if i == 1:
             return "XL"
         return "CD"
     
-    def _getSymbolCase5(self, i: int) -> str:
+    def __getSymbolCase5(self, i: int) -> str:
         if i == 0:
             return "V"
         if i == 1:
             return "L"
         return "D"
     
-    def _getSymbolCase9(self, i: int) -> str:
+    def __getSymbolCase9(self, i: int) -> str:
         if i == 0:
             return "IX"
         if i == 1:
