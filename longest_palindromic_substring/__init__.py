b7baf5ab7f93449d4c8e209b1fce6b880370f153
mic-00 <michele.masetto@gmail.com>
2026-01-30 10:50:02 +0100

    Solve LeetCode "Longest Palindromic Substring" (https://leetcode.com/problems/longest-palindromic-substring)

--- longest_palindromic_substring/__init__.py
+++ longest_palindromic_substring/__init__.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def longestPalindrome(self, s: str) -> str:
         def longestPalindromeRecursive(s, i, j):
-            if self._isPalindrome(s[i:j]):
+            if self.__isPalindrome(s[i:j]):
                 return s[i:j]
             s1, s2 = longestPalindromeRecursive(s, i, j - 1), longestPalindromeRecursive(s, i + 1, j)
             return max(s1, s2, key=len)
         return longestPalindromeRecursive(s, 0, len(s))
     
-    def _isPalindrome(self, s: str) -> bool:
+    def __isPalindrome(self, s: str) -> bool:
         if len(s) == 0:
             return True
-        return s[0] == s[-1] and self._isPalindrome(s[1:-1])
+        return s[0] == s[-1] and self.__isPalindrome(s[1:-1])
